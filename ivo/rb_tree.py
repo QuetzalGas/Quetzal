a34f123b2bb35c952950e0e213ed8d0539dcdc5b
Ivo C.S. Wingelaar <ivo.wingelaar@gmail.com>
2017-12-03 07:06:03 +0100

    Prototype RB-tree

---
+++ ivo/rb_tree.py
@@ -0,0 +1,352 @@
+class Node:
+    def __init__(self, content):
+        self.content = content
+        self.black = False
+        self.left = None
+        self.right = None
+        self.parent = None
+
+    def is_black(self):
+        return self.black
+
+    def is_red(self):
+        return not self.black
+
+    def is_four_node(self):
+        return (self.left is not None and not self.left.black) and\
+               (self.right is not None and not self.right.black)
+
+    def flip_color(self):
+        self.black = not self.black
+        self.left.black = not self.left.black
+        self.right.black = not self.left.black
+
+    def is_two_node(self):
+        if not self.black:
+            return False
+
+        return (self.left is not None and self.left.black) and\
+               (self.right is not None and self.right.black)
+
+    def is_left_child(self):
+        return (self.parent is not None) and (self.parent.left == self)
+
+    def is_right_child(self):
+        return (self.parent is not None) and (self.parent.right == self)
+
+    def set_right_child(self, child):
+        previous = self.right
+
+        self.right = child
+        if self.right is not None:
+            self.right.parent = self
+
+        if previous is not None:
+            previous.parent = None
+        
+        return previous
+
+    def rotate_left(self):
+        """ Rotate this node to the left, and return the highest node after rotation.
+
+        A rotation is a local O(1) operation that restructures nodes such that
+        one subtree gets closer the root, and another subtree gets one level
+        further from the root. This operation preserves the in-order property
+        of the RB-tree.
+
+        This is used to rotate a right leaning 3-node to a left leaning 3-
+        node.
+
+            self                           x
+         a        x          =>      self       c
+               b     c            a        b
+        """
+        parent = self.parent
+        x = self.right
+
+        self.right = x.left
+        self.right.parent = self
+
+        x.left = self
+        x.left.parent = x
+
+        x.parent = parent
+        if parent is not None:
+            if parent.left == self:
+                parent.left = x
+            elif parent.right == self:
+                parent.right = x
+            else:
+                raise RuntimeError("Invariant broken")
+
+        x.black = self.black
+        self.black = False
+        return x
+
+    def rotate_right(self):
+        """
+               self           x
+            x        c  => a     self
+         a     b              b        c
+        """
+        parent = self.parent
+        x = self.left
+
+        self.left = x.right
+        self.left.parent = self
+
+        x.right = self
+        x.right.parent = x
+
+        x.parent = parent
+        if parent is not None:
+            if parent.left == self:
+                parent.left = x
+            elif parent.right == self:
+                parent.right = x
+            else:
+                raise RuntimeError("Invariant broken")
+
+        x.black = self.black
+        self.black = False
+        return x
+
+
+    def set_left_child(self, child):
+        previous = self.left
+
+        self.left = child
+        if self.left is not None:
+            self.left.parent = self
+
+        if previous is not None:
+            previous.parent = None
+
+        return previous
+
+
+    def split(self):
+        if not self.is_four_node():
+            return
+
+        if self.parent is None:
+            # Root
+            self.left.black = True
+            self.right.black = True
+        else:
+
+            if self.parent.is_two_node():
+                self.black = False
+                self.left.black = True
+                self.right.black = True
+            else:
+                # 3-node parent
+                if self.is_left_child():
+                    if not self.parent.right.black:
+                        self.black = False
+                        self.left.black = True
+                        self.right.black = True
+                    elif self.parent.is_left_child():
+                        P = self.parent
+                        Q = P.parent
+
+                        f = Q.set_right_child(P)
+                        Q.set_left_child(self)
+                        
+                        self.black = False
+                        self.left.black = True
+                        self.right.black = True
+
+                        q = Q.content
+                        Q.content = P.content
+                        P.content = q
+
+                        e = P.set_right_child(f)
+                        P.set_left_child(e)
+                    else:
+                        # The parent is a right child.
+                        Q = self.parent
+                        P = Q.parent
+
+                        a = P.set_left_child(self)
+                        self.black = False
+
+                        p = P.content
+                        P.content = self.content
+                        self.content = p
+
+                        S = self.set_left_child(a)
+                        L = self.set_right_child(S)
+                        S.black = True
+
+                        Q.set_left_child(L)
+                        L.black = True
+                else:
+                    # We are a right child.
+                    if not self.parent.left.black:
+                        self.black = False
+                        self.left.black = True
+                        self.right.black = True
+                    elif self.parent.is_right_child():
+                        Q = self.parent
+                        P = Q.parent
+
+                        a = P.set_left_child(Q)
+                        P.set_right_child(self)
+
+                        self.black = False
+                        self.left.black = False
+                        self.right.black = False
+
+                        p = P.content
+                        P.content = Q.content
+                        Q.content = p
+
+                        b = Q.set_left_child(a)
+                        Q.set_right_child(b)
+                    else:
+                        # The parent is a left child
+                        pass
+
+    def insert(self, content):
+        self.split()
+
+        if content > self.content:
+            if self.right is None:
+                self.set_right_child(Node(content))
+                self.right.black = False
+            else:
+                self.right.insert(content)
+        else:
+            if self.left is None:
+                self.set_left_child(Node(content))
+                self.left.black = False
+            else:
+                self.left.insert(content)
+
+    def dot(self, c=0):
+        if self.content is None:
+            return None
+
+        if self.black:
+            output = '  node{}[label = "<l>|<m> {}|<r>"];\n'.format(c, self.content)
+        else:
+            output = '  node{}[label = "<l>|<m> {}|<r>", color=red];\n'.format(c, self.content)
+
+        root = c
+
+        if (self.left is not None) and (self.left.content is not None):
+            (o2, c) = self.left.dot(c+1) 
+            output += o2
+
+            if self.left.black:
+                output += '  "node{}":l->"node{}":m;\n'.format(root, root+1)
+            else:
+                output += '  "node{}":l->"node{}":m [color=red];\n'.format(root, root+1)
+        else:
+            output += '  node{} [style="invisible"];\n'.format(root+1)
+            output += '  "node{}":l->node{} [style="invisible"];\n'.format(root, root+1)
+            c = c+1
+
+        if (self.right is not None) and (self.right.content is not None):
+            c_plus = c+1
+            (o2, c) = self.right.dot(c_plus)
+            output += o2
+
+            if self.right.black:
+                output += '  "node{}":r->"node{}":m;\n'.format(root, c_plus)
+            else:
+                output += '  "node{}":r->"node{}":m [color=red];\n'.format(root, c_plus)
+
+        else:
+            output += '  node{} [style="invisible"];\n'.format(c+1)
+            output += '  "node{}":r->node{} [style="invisible"];\n'.format(root, c+1)
+            c = c+1
+
+        return (output, c+1)
+
+def standard():
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+
+    S = Node("S")
+    S.black = False
+    S.set_left_child(a)
+    S.set_right_child(b)
+
+    L = Node("L")
+    L.black = False
+    L.set_left_child(c)
+    L.set_right_child(d)
+
+    M = Node("M")
+    M.set_left_child(S)
+    M.set_right_child(L)
+
+    return M
+
+def b2():
+    M = standard()
+
+    e = Node("e")
+    P = Node("P")
+    P.set_right_child(e)
+    P.set_left_child(M)
+
+    return P
+
+def b4():
+    M = standard()
+
+    e = Node("e")
+    f = Node("f")
+    P = Node("P")
+    P.set_right_child(e)
+    P.set_left_child(M)
+    P.black = False
+    Q = Node("Q")
+    Q.set_left_child(P)
+    Q.set_right_child(f)
+
+    return Q
+
+def b6():
+    M = standard()
+
+    P = Node("P")
+    P.set_left_child(Node("x"))
+
+    Q = Node("Q")
+    Q.black = False
+    Q.set_right_child(Node("f"))
+    Q.set_left_child(M)
+    P.set_right_child(Q)
+
+    return P
+
+def insert():
+    R = Node(4)
+    R.insert(3)
+    R.insert(2)
+
+    while R.parent is not None:
+        R = R.parent
+
+    return R
+
+rb = b6()
+
+def print_rb(rb, filename):
+    with open(filename, 'w') as of:
+        of.write('digraph rb {\n')
+        of.write('  node[shape = record];\n')
+        out = rb.dot()
+        of.write(out[0])
+        of.write('}')
+
+
+print_rb(rb, "pre.dot")
+rb.right.left.split()
+print_rb(rb, "post.dot")
